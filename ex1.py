1ed3c4e14afac8faac339bdb524c63896061d038
basicworld <basicworld@163.com>
2016-12-28 14:18:18 +0800

    1.0

---
+++ ex1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+拓展shapfile
+构建填充色块的功能
+"""
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')  # 编译环境utf8
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from mpl_draw_patch import mpl_draw_patch, shapefiles_DIR
+
+
+def test():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # 构建地图实例
+    m = Basemap(projection='merc',
+                llcrnrlon=73,
+                llcrnrlat=15,
+                urcrnrlon=135,
+                urcrnrlat=55,
+                resolution='c',
+                lat_0=38.5,
+                lon_0=95)
+    filename = os.path.join(shapefiles_DIR, 'CHN_adm_shp/CHN_adm1')
+    mpl_draw_patch(ax, m, filename, ('_all', None))
+    plt.show()
+
+
+if __name__ == '__main__':
+    test()
