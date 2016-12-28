801086704243caa08acba65fde106046537dfeca
basicworld <basicworld@163.com>
2016-12-28 15:51:44 +0800

    debug

--- mpl_draw_patch.py
+++ mpl_draw_patch.py
@@ -50,3 +50,28 @@ def mpl_draw_patch(ax,
                           facecolor=facecolor,
                           edgecolor=edgecolor,
                           linewidth=linewidth))
+
+
+def test():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # 构建地图实例
+    m = Basemap(projection='merc',
+                llcrnrlon=73,
+                llcrnrlat=18,
+                urcrnrlon=135,
+                urcrnrlat=54,
+                resolution='c',
+                lat_0=38.5,
+                lon_0=95)
+    filename = os.path.join(shapefiles_DIR, 'CHN_adm_shp/CHN_adm1')
+    mpl_draw_patch(ax, m, filename, ('_all', None))
+    mpl_draw_patch(ax, m, filename, ('NAME_1', 'Beijing'), facecolor='#F52670')
+    fig.set_size_inches(15, 12)
+    fig.savefig('test.png', dpi=72, bbox_inches='tight')
+    # plt.show()
+
+
+if __name__ == '__main__':
+    test()
