b96535fa94eeddb221d5693e4edf421519f8c149
basicworld <basicworld@163.com>
2016-12-28 15:33:14 +0800

    a big pic

--- test.py
+++ test.py
@@ -8,6 +8,10 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')  # 编译环境utf8
 
+# # https://support.enthought.com/hc/en-us/articles/207291386-Matplotlib-1-5-1-Font-Cache-Warning
+# import matplotlib as mpl
+# font_cache_path = mpl.get_cachedir() + '/fontList.cache'
+# os.remove(font_cache_path)
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from mpl_draw_patch import mpl_draw_patch, shapefiles_DIR
@@ -28,7 +32,10 @@ def test():
                 lon_0=95)
     filename = os.path.join(shapefiles_DIR, 'CHN_adm_shp/CHN_adm1')
     mpl_draw_patch(ax, m, filename, ('_all', None))
-    plt.show()
+    mpl_draw_patch(ax, m, filename, ('NAME_1', 'Beijing'), facecolor='#F52670')
+    fig.set_size_inches(50, 40)
+    fig.savefig('test.png', dpi=72, bbox_inches='tight')
+    # plt.show()
 
 
 if __name__ == '__main__':
